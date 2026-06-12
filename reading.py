5e43c22ad20d907c2d7ef66e0b5537e4bf7d0b11
lionheart <mikeoladapo2004@gmail.com>
2026-06-12 11:08:09 +0100

    steady

--- reading.py
+++ reading.py
@@ -3,8 +3,8 @@ import pandas as pd
 df = pd.read_csv("riders.csv")
 lagos_rider = df[df["city"]=="Lagos"]
 earnings = df["daily_earnings_ngn"].mean()
-active_riders = df[df["status"] == "active"]
-print(lagos_rider)
-#print(active_riders)
+active_riders = df[df["status"] == "active"][df["zone"]=="Surulere"]
+#print(lagos_rider)
+print(active_riders)
 #print(earnings)
 
